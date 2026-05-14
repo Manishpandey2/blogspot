dd12e9f68b3fcdecc3d9df2dbf65d7380d1c1a63
Manishpande2 <biraalogang@gmail.com>
2026-05-14 17:32:42 +0545

    Project setup, mvc pattern follow garna khojeko xa navbar lai xuttai component bannaera rakheko xa ani css lai pani tesai gareko xa

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/blogs", (req, res) => {
+  res.render("blogs");
+});
+
+app.get("/categories", (req, res) => {
+  res.render("categorie");
+});
+
+app.use(express.static("public/css/"));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`project has been started at port no ${PORT}`);
+});
