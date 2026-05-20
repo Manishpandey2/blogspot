d4de2d3c4858f21178d1ae9013b16ba8d3af2c00
Manishpande2 <biraalogang@gmail.com>
2026-05-20 16:27:07 +0545

    user log in feature implemented

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const { users } = require("./model/index");
 const bcrypt = require("bcrypt");
+const { where } = require("sequelize");
 
 require("./model/index");
 const app = express();
@@ -66,6 +67,40 @@ app.post("/register", async (req, res) => {
     console.log(error);
     res.status(400).json({
       message: "unable to create user",
+    });
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "both fields are required",
+      });
+    }
+    const user = await users.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      return res.status(400).json({
+        message: "user not found with this email",
+      });
+    }
+    const isMatched = bcrypt.compareSync(password, user.password);
+    if (!isMatched) {
+      return res.status(401).json({
+        message: "wrong password",
+      });
+    }
+    return res.status(200).json({
+      message: "User Logged in",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "server error",
     });
   }
 });
