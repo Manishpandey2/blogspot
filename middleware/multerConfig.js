01ae041370d8c30547302c9418a408a8ff98827c
Manishpande2 <biraalogang@gmail.com>
2026-06-01 16:54:03 +0545

    file validation mimetype

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -2,6 +2,12 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const uploadedFileType = ["image/png", "image/jpg", "image/jpeg"];
+    if (!uploadedFileType.includes(file.mimetype)) {
+      cb(new Error("Invalid file types, Only supports png, jpg, jpeg file"));
+      return;
+    }
+
     cb(null, "./storage");
   },
   filename: function (req, file, cb) {
