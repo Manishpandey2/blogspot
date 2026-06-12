238dbf27f1aa027e33cd0bce1b2b8b553899102a
Manishpande2 <biraalogang@gmail.com>
2026-06-12 19:51:57 +0545

    auth flash message setup (still need to test)

--- controller/authController.js
+++ controller/authController.js
@@ -6,11 +6,17 @@ const sendEmail = require("../services/sendOtp");
 
 exports.renderadminDashboard = async (req, res) => {
   const blog = await blogs.findAll();
-  res.render("admindashboard", { blog });
+  const error = req.flash("error");
+  const success = req.flash("success");
+  res.render("admindashboard", { blog, error, success });
 };
 
 exports.renderLogin = (req, res) => {
-  res.render("auth/login");
+  const error = req.flash("error");
+
+  const success = req.flash("success");
+
+  res.render("auth/login", { error, success });
 };
 exports.renderRegister = (req, res) => {
   const error = req.flash("error");
@@ -65,9 +71,11 @@ exports.userLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).json({
-        message: "both fields are required",
-      });
+      // return res.status(400).json({
+      //   message: "both fields are required",
+      // });
+      req.flash("error", "Both fields are required");
+      return res.redirect("/login");
     }
     const user = await users.findOne({
       where: {
@@ -75,15 +83,19 @@ exports.userLogin = async (req, res) => {
       },
     });
     if (!user) {
-      return res.status(400).json({
-        message: "user not found with this email",
-      });
+      // return res.status(400).json({
+      //   message: "user not found with this email",
+      // });
+      req.flash("error", "User not found with this email");
+      return res.redirect("/login");
     }
     const isMatched = await bcrypt.compare(password, user.password);
     if (!isMatched) {
-      return res.status(401).json({
-        message: "wrong password",
-      });
+      // return res.status(401).json({
+      //   message: "wrong password",
+      // });
+      req.flash("error", "Wrong Password !!");
+      return res.redirect("/login");
     }
     const token = jwt.sign(
       { id: user.id, userEmail: user.email },
@@ -99,29 +111,36 @@ exports.userLogin = async (req, res) => {
     // return res.status(200).json({
     //   message: "User Logged in",
     // });
-    res.redirect("/blogs");
+    req.flash("success", "User Logged In");
+    return res.redirect("/blogs");
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "server error",
-    });
+    // res.status(500).json({
+    //   message: "server error",
+    // });
+    req.flash("error", "Server Error");
+    return res.redirect("/login");
   }
 };
 
 exports.logout = (req, res) => {
   res.clearCookie("token");
-  res.redirect("/");
+  return res.redirect("/");
 };
 
 exports.getForgotPassword = (req, res) => {
-  res.render("auth/forgotPassword");
+  const error = req.flash("error");
+  const success = req.flash("success");
+  res.render("auth/forgotPassword", { success, error });
 };
 
 exports.postForgotPassword = async (req, res) => {
   try {
     const { email } = req.body;
     if (!email) {
-      return res.send("please provide email");
+      // return res.send("please provide email");
+      req.flash("error", "Please Provide an email");
+      return res.redirect("/forgotPassword");
     }
 
     const existingUser = await users.findOne({
@@ -130,7 +149,9 @@ exports.postForgotPassword = async (req, res) => {
       },
     });
     if (!existingUser) {
-      return res.status(400).send("invalid email address");
+      // return res.status(400).send("invalid email address");
+      req.flash("error", "Invalid email");
+      return res.redirect("/forgotPassword");
     }
     const OTP = Math.floor(100000 + Math.random() * 900000);
     existingUser.otp = OTP;
@@ -142,16 +163,20 @@ exports.postForgotPassword = async (req, res) => {
       subject: "Forgot Password OTP",
       otp: OTP,
     });
-    res.redirect("/verifyOtp?email=" + email);
+    req.flash("success", "OTP sent to your gmail");
+    return res.redirect("/verifyOtp?email=" + email);
   } catch (error) {
     console.log(error);
-    res.status(500).send("Server Error");
+    // res.status(500).send("Server Error");
+    req.flash("error", "Server Error");
+    return res.redirect("/forgotPassword");
   }
 };
 exports.getverifyOtp = (req, res) => {
   const email = req.query.email;
-
-  res.render("auth/verifyOtp", { email: email });
+  const error = req.flash("error");
+  const success = req.flash("success");
+  res.render("auth/verifyOtp", { email: email, error, success });
 };
 exports.verifyOtp = async (req, res) => {
   try {
@@ -159,7 +184,9 @@ exports.verifyOtp = async (req, res) => {
     const email = req.params.id;
 
     if (!otp || !email) {
-      return res.send("You need to provide email and otp");
+      // return res.send("You need to provide email and otp");
+      req.flash("error", "You need to provide otp and email");
+      return res.redirect("/verifyOtp");
     }
     const user = await users.findOne({
       where: {
@@ -168,7 +195,9 @@ exports.verifyOtp = async (req, res) => {
       },
     });
     if (!user) {
-      return res.send("Invalid Otp");
+      // return res.send("Invalid Otp");
+      req.flash("error", "Invalide OTP");
+      return res.redirect("/verifyOtp");
     } else {
       const currentTime = Date.now();
       const expiryTime = user.otpExpiry;
@@ -176,33 +205,44 @@ exports.verifyOtp = async (req, res) => {
         user.otp = null;
         user.otpExpiry = null;
         await user.save();
-        res.send("Your Otp is expired");
+        // res.send("Your Otp is expired");
+        req.flash("error", "Your OTP is expired");
+        return res.redirect("/verifyOtp");
       } else {
         // res.send("OTP verified");
         user.isOtpverified = true;
         await user.save();
-        res.redirect("/changePassword?email=" + email);
+        req.flash("success", "OTP Verfied");
+        return res.redirect("/changePassword?email=" + email);
       }
     }
   } catch (error) {
     console.log(error);
-    res.send("Server Error");
+    // res.send("Server Error");
+    req.flash("error", "Server Errror");
+    return res.redirect("/verifyOtp");
   }
 };
 
 exports.getChangePassword = (req, res) => {
   const email = req.query.email;
-  res.render("auth/changePassword", { email: email });
+  const error = req.flash("error");
+  const success = req.flash("success");
+  res.render("auth/changePassword", { email: email, error, success });
 };
 
 exports.handleChangePassword = async (req, res) => {
   try {
     const { email, newPassword, newConfirmPassword } = req.body;
     if (!email || !newPassword || !newConfirmPassword) {
-      return res.send("Please provide email, newPassword and ConfirmPassowrd");
+      // return res.send("Please provide email, newPassword and ConfirmPassowrd");
+      req.flash("error", "All the feilds are required");
+      return res.redirect("/changePassword");
     }
     if (newPassword !== newConfirmPassword) {
-      return res.send("Your password and confirm password is not matching");
+      // return res.send("Your password and confirm password is not matching");
+      req.flash("error", "password and confirm password do not match");
+      return res.redirect("/changePassword");
     }
     const user = await users.findOne({
       where: {
@@ -210,7 +250,9 @@ exports.handleChangePassword = async (req, res) => {
       },
     });
     if (!user) {
-      return res.send("User do not found");
+      // return res.send("User do not found");
+      req.flash("error", "User is not Found");
+      return res.redirect("/changePassword");
     }
     if (!user.isOtpverified) {
       return res.send("Please verify OTP at first");
@@ -220,9 +262,12 @@ exports.handleChangePassword = async (req, res) => {
     user.otpExpiry = null;
     user.isOtpverified = false;
     await user.save();
+    req.flash("success", "Password Changed");
     return res.redirect("/login");
   } catch (error) {
     console.log(error);
-    res.status(500).send("Server Error");
+    // res.status(500).send("Server Error");
+    req.flash("error", "Server Error");
+    return res.redirect("/changePassword");
   }
 };
